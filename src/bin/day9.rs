4cb0d76ddcf18e6411643aaf7b0e79d229cf5a4a
gjf2a <ferrer@hendrix.edu>
2022-12-09 00:11:05 -0600

    refactored a little.

--- src/bin/day9.rs
+++ src/bin/day9.rs
@@ -4,24 +4,28 @@ use advent_code_lib::{simpler_main, Position, all_lines, Dir};
 
 fn main() -> anyhow::Result<()> {
     simpler_main(|filename| {
-        let mut head = Position::new();
-        let mut tail = Position::new();
-        let mut tails = BTreeSet::new();
-        tails.insert(tail);
-        for line in all_lines(filename)? {
-            let (dir, reps) = parse_line(line.as_str())?;
-            for _ in 0..reps {
-                let last_head = head;
-                head.update(dir);
-                if tail != head && head.neighbors().all(|n| n != tail) {
-                    tail = last_head;
-                }
-                tails.insert(tail);
-            }
-        }
-        println!("Part 1: {}", tails.len());
+        println!("Part 1: {}", part1(filename)?);
         Ok(())
     })
+}
+
+fn part1(filename: &str) -> anyhow::Result<usize> {
+    let mut head = Position::new();
+    let mut tail = Position::new();
+    let mut tails = BTreeSet::new();
+    tails.insert(tail);
+    for line in all_lines(filename)? {
+        let (dir, reps) = parse_line(line.as_str())?;
+        for _ in 0..reps {
+            let last_head = head;
+            head.update(dir);
+            if tail != head && head.neighbors().all(|n| n != tail) {
+                tail = last_head;
+            }
+            tails.insert(tail);
+        }
+    }
+    Ok(tails.len())
 }
 
 fn parse_line(line: &str) -> anyhow::Result<(Dir, usize)> {
