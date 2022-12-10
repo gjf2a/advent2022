fd8f8dfd40294cd41581828ef919c3059dc425c3
gjf2a <ferrer@hendrix.edu>
2022-12-10 07:41:57 -0600

    Continued progress on Day 9, Part 2, but still doesn''t work.

--- src/bin/day9.rs
+++ src/bin/day9.rs
@@ -4,10 +4,18 @@ use advent_code_lib::{simpler_main, Position, all_lines, Dir};
 
 fn main() -> anyhow::Result<()> {
     simpler_main(|filename| {
-        println!("Part 1: {}", tail_visit_count::<2>(filename)?);
-        println!("Part 2: {}", tail_visit_count::<10>(filename)?);
+        println!("Part 1a: {}", part1(filename)?);
+        println!("Part 1b: {}", tail_visit_count::<2>(filename)?);
+        println!("Part 2:  {}", tail_visit_count::<10>(filename)?);
         Ok(())
     })
+}
+
+fn print_array<const N: usize>(array: &[Position; N]) {
+    for p in array.iter() {
+        print!(" ({},{})", p.row, p.col);
+    }
+    println!();
 }
 
 fn part1(filename: &str) -> anyhow::Result<usize> {
@@ -33,7 +41,7 @@ fn tail_visit_count<const N: usize>(filename: &str) -> anyhow::Result<usize> {
     let mut tails = BTreeSet::new();
     for line in all_lines(filename)? {
         let (dir, reps) = parse_line(line.as_str())?;
-        for _ in 0..reps {
+        for r in 0..reps {
             let last_rope = rope;
             rope[0].update(dir);
             for i in 1..N {
@@ -42,6 +50,8 @@ fn tail_visit_count<const N: usize>(filename: &str) -> anyhow::Result<usize> {
                 }
             }
             tails.insert(rope[N - 1]);
+            print!("{dir:?} {}", r + 1);
+            print_array(&rope);
         }
     }
     Ok(tails.len())
