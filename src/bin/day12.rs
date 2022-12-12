d5598f8f6b87bdd495a5fc05d2874ac459238b26
gjf2a <ferrer@hendrix.edu>
2022-12-12 10:11:04 -0600

    Day 12 complete!

---
+++ src/bin/day12.rs
@@ -0,0 +1,49 @@
+use std::cmp::min;
+
+use advent_code_lib::{GridCharWorld, simpler_main, breadth_first_search, Position, ParentMap, SearchQueue, ContinueSearch};
+
+fn main() -> anyhow::Result<()> {
+    simpler_main(|filename| {
+        let map = GridCharWorld::from_char_file(filename)?;
+        println!("Part 1: {}", part1(&map));
+        println!("Part 2: {}", part2(&map));
+        Ok(())
+    })
+}
+
+fn height_of(c: char) -> char {
+    match c {
+        'S' => 'a',
+        'E' => 'z',
+        _ => c
+    }
+}
+
+fn part1(map: &GridCharWorld) -> usize {
+    distance_to_goal_from(map, map.any_position_for('S')).unwrap()
+}
+
+fn climb_distance(start: char, end: char) -> i8 {
+    end as i8 - start as i8
+}
+
+fn distance_to_goal_from(map: &GridCharWorld, start: Position) -> Option<usize> {
+    let end = map.any_position_for('E');
+    let parents: ParentMap<Position> = breadth_first_search(&start, |p, q| {
+        let p_height = height_of(map.value(*p).unwrap());
+        for neighbor in p.manhattan_neighbors() {
+            let distance = map.value(neighbor).map(|h| climb_distance(p_height, height_of(h)));
+            if let Some(distance) = distance {
+                if distance <= 1 {
+                    q.enqueue(&neighbor);
+                }
+            }
+        }
+        ContinueSearch::Yes
+    });
+    parents.path_back_from(&end).map(|path_back| path_back.len() - 1)
+}
+
+fn part2(map: &GridCharWorld) -> usize {
+    min(part1(map), map.positions_for('a').iter().filter_map(|start| distance_to_goal_from(map, *start)).min().unwrap())
+}
--- src/bin/day8.rs
+++ src/bin/day8.rs
@@ -1,22 +1,22 @@
-use advent_code_lib::{simpler_main, DirType, GridWorld, ManhattanDir, Position};
+use advent_code_lib::{simpler_main, DirType, GridDigitWorld, ManhattanDir, Position};
 use enum_iterator::*;
 
 fn main() -> anyhow::Result<()> {
     simpler_main(|filename| {
-        let map = GridWorld::from_file(filename)?;
+        let map = GridDigitWorld::from_digit_file(filename)?;
         println!("Part 1: {}", part1(&map));
         println!("Part 2: {}", part2(&map));
         Ok(())
     })
 }
 
-fn part1(map: &GridWorld) -> usize {
+fn part1(map: &GridDigitWorld) -> usize {
     map.position_iter()
         .filter(|p| all::<ManhattanDir>().any(|dir| find_blocking_tree(map, *p, dir).is_none()))
         .count()
 }
 
-fn find_blocking_tree(map: &GridWorld, p: Position, dir: ManhattanDir) -> Option<Position> {
+fn find_blocking_tree(map: &GridDigitWorld, p: Position, dir: ManhattanDir) -> Option<Position> {
     let tree_height = map.value(p).unwrap();
     let mut p = dir.next_position(p);
     while let Some(blocker) = map.value(p) {
@@ -28,14 +28,14 @@ fn find_blocking_tree(map: &GridWorld, p: Position, dir: ManhattanDir) -> Option
     None
 }
 
-fn part2(map: &GridWorld) -> usize {
+fn part2(map: &GridDigitWorld) -> usize {
     map.position_iter()
         .map(|p| scenic_score(map, p))
         .max()
         .unwrap()
 }
 
-fn scenic_score(map: &GridWorld, p: Position) -> usize {
+fn scenic_score(map: &GridDigitWorld, p: Position) -> usize {
     all::<ManhattanDir>()
         .map(|dir| {
             find_blocking_tree(map, p, dir).map_or_else(
