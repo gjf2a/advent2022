508662166963628e0e07ebb7724d9ec428b342e5
gjf2a <ferrer@hendrix.edu>
2022-12-12 22:09:47 -0600

    Refactored Day 7

--- src/bin/day7.rs
+++ src/bin/day7.rs
@@ -11,11 +11,32 @@ fn main() -> anyhow::Result<()> {
     })
 }
 
+#[derive(Debug, Clone)]
+pub enum FileEntry {
+    File(usize),
+    Directory(Option<usize>, BTreeMap<String, usize>),
+}
+
+impl FileEntry {
+    pub fn is_directory(&self) -> bool {
+        match self {
+            FileEntry::File(_) => false,
+            FileEntry::Directory(_, _) => true,
+        }
+    }
+
+    pub fn size(&self, system: &FileSystem) -> usize {
+        match self {
+            FileEntry::File(size) => *size,
+            FileEntry::Directory(_, children) => 
+            children.values().map(|i| system.inode2object[*i].size(system)).sum(),
+        }
+    }
+}
+
 #[derive(Debug, Default)]
 pub struct FileSystem {
-    next_inode: usize,
-    directories: BTreeMap<usize, (Option<usize>, BTreeMap<String, usize>)>,
-    files: BTreeMap<usize, usize>,
+    inode2object: Vec<FileEntry>
 }
 
 impl FileSystem {
@@ -24,19 +45,18 @@ impl FileSystem {
         let mut current_dir = system.new_directory(None);
         for line in all_lines(filename)? {
             if line == "$ cd .." {
-                let (parent, _) = system.directories.get(&current_dir).unwrap();
-                current_dir = parent.unwrap();
+                if let FileEntry::Directory(parent, _) = system.inode2object[current_dir] {
+                    if let Some(parent) = parent {
+                        current_dir = parent;
+                    }
+                }
             } else if line == "$ cd /" {
                 current_dir = 0;
             } else if line.starts_with("$ cd") {
-                let dir_name = line.split_whitespace().skip(2).next().unwrap();
-                current_dir = *system
-                    .directories
-                    .get(&current_dir)
-                    .unwrap()
-                    .1
-                    .get(dir_name)
-                    .unwrap();
+                if let FileEntry::Directory(_, children) = &system.inode2object[current_dir] {
+                    let dir_name = line.split_whitespace().skip(2).next().unwrap();
+                    current_dir = children.get(dir_name).copied().unwrap();
+                }
             } else if line == "$ ls" {
                 // Intentionally blank
             } else {
@@ -48,61 +68,40 @@ impl FileSystem {
                 } else {
                     system.new_file(info.parse::<usize>().unwrap())
                 };
-                system
-                    .directories
-                    .get_mut(&current_dir)
-                    .unwrap()
-                    .1
-                    .insert(name.to_owned(), id);
+                if let FileEntry::Directory(_, children) = &mut system.inode2object[current_dir] {
+                    children.insert(name.to_owned(), id);
+                }
             }
         }
         Ok(system)
     }
 
     fn new_directory(&mut self, parent: Option<usize>) -> usize {
-        let id = self.new_id();
-        self.directories.insert(id, (parent, BTreeMap::new()));
+        let id = self.inode2object.len();
+        self.inode2object.push(FileEntry::Directory(parent, BTreeMap::new()));
         id
     }
 
     fn new_file(&mut self, size: usize) -> usize {
-        let id = self.new_id();
-        self.files.insert(id, size);
+        let id = self.inode2object.len();
+        self.inode2object.push(FileEntry::File(size));
         id
     }
 
-    fn new_id(&mut self) -> usize {
-        let id = self.next_inode;
-        self.next_inode += 1;
-        id
-    }
-
-    pub fn size_of(&self, id: usize) -> usize {
-        match self.files.get(&id) {
-            Some(size) => *size,
-            None => match self.directories.get(&id) {
-                None => panic!("This shouldn't happen!"),
-                Some((_, files)) => files.iter().map(|(_, id)| self.size_of(*id)).sum(),
-            },
-        }
+    pub fn directory_sizes(&self) -> Vec<usize> {
+        self.inode2object.iter().filter(|entry| entry.is_directory()).map(|dir| dir.size(self)).collect()
     }
 
     pub fn part1(&self) -> usize {
-        self.directories
-            .keys()
-            .map(|k| self.size_of(*k))
-            .filter(|s| *s <= 100000)
-            .sum()
+        let sizes = self.directory_sizes();
+        sizes.iter().filter(|s| **s <= 100000).sum()
     }
 
     pub fn part2(&self) -> usize {
         const TOTAL_SPACE: usize = 70000000;
         const SPACE_NEEDED: usize = 30000000;
-        let mut sizes = self.directories.keys().map(|k| self.size_of(*k));
-        let space_available = TOTAL_SPACE - sizes.next().unwrap();
-        sizes
-            .filter(|s| space_available + *s >= SPACE_NEEDED)
-            .min()
-            .unwrap()
+        let sizes = self.directory_sizes();
+        let space_available = TOTAL_SPACE - sizes[0];
+        sizes.iter().skip(1).filter(|s| space_available + *s >= SPACE_NEEDED).min().copied().unwrap()
     }
 }
