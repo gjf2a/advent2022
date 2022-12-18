6d987148d162315bd825579c3f89e5d2bde2af48
gjf2a <ferrer@hendrix.edu>
2022-12-18 04:34:52 -0600

    Day 18, Part 2: Not correct.

--- src/bin/day18.rs
+++ src/bin/day18.rs
@@ -1,13 +1,28 @@
 use advent_code_lib::{all_lines, simpler_main, Point};
 
-type Cubito = Point<i64, 3>;
+const CUBE_DIM: usize = 3;
+
+type Cubito = Point<i64, CUBE_DIM>;
 
 fn main() -> anyhow::Result<()> {
     simpler_main(|filename| {
         let droplet = Droplet::from_file(filename)?;
-        println!("Part 1: {}", droplet.surface_area());
+        println!("Part 1: {}", part1(&droplet));
+        println!("Part 2: {}", part2(&droplet));
         Ok(())
     })
+}
+
+pub fn part1(droplet: &Droplet) -> usize {
+    droplet.surface_area(|a, b| a.adjacent(b))
+}
+
+pub fn eventually_adjacent(a: &Cubito, b: &Cubito) -> bool {
+    (0..CUBE_DIM).filter(|i| a.coords[*i] == b.coords[*i]).count() == CUBE_DIM - 1
+}
+
+pub fn part2(droplet: &Droplet) -> usize {
+    droplet.surface_area(eventually_adjacent)
 }
 
 pub struct Droplet {
@@ -23,10 +38,10 @@ impl Droplet {
         Ok(Self { cubes })
     }
 
-    pub fn surface_area(&self) -> usize {
+    pub fn surface_area<T: Fn(&Cubito,&Cubito)->bool>(&self, test: T) -> usize {
         let mut result = 0;
         for cube in self.cubes.iter() {
-            let in_contact = self.cubes.iter().filter(|c| cube.adjacent(c)).count();
+            let in_contact = self.cubes.iter().filter(|c| test(cube, c)).count();
             assert!(in_contact <= 6);
             result += 6 - in_contact;
         }
@@ -36,11 +51,11 @@ impl Droplet {
 
 #[cfg(test)]
 mod tests {
-    use crate::Droplet;
+    use crate::{Droplet, part1};
 
     #[test]
     fn test_1() {
         let droplet = Droplet::from_file("ex/day18.txt").unwrap();
-        assert_eq!(droplet.surface_area(), 64);
+        assert_eq!(part1(&droplet), 64);
     }
 }
