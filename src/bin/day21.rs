43037f637cdabc34344f5e307ab92c2b89e644e7
gjf2a <ferrer@hendrix.edu>
2022-12-21 09:37:06 -0600

    Part 2: Works on example, but it seems too slow for the puzzle input.

--- src/bin/day21.rs
+++ src/bin/day21.rs
@@ -1,4 +1,4 @@
-use std::{collections::BTreeMap, str::FromStr};
+use std::{collections::{BTreeMap, BTreeSet, VecDeque}, str::FromStr};
 
 use advent_code_lib::{all_lines, simpler_main};
 use anyhow::bail;
@@ -6,6 +6,7 @@ use anyhow::bail;
 fn main() -> anyhow::Result<()> {
     simpler_main(|filename| {
         println!("Part 1: {}", part1(filename)?);
+        println!("Part 2: {}", part2(filename)?);
         Ok(())
     })
 }
@@ -15,20 +16,29 @@ pub fn part1(filename: &str) -> anyhow::Result<i64> {
     Ok(troop.evaluate_root())
 }
 
-/*pub fn part2(filename: &str) -> anyhow::Result<i64> {
+pub fn part2(filename: &str) -> anyhow::Result<i64> {
     let troop = MonkeyTroop::from_file(filename)?;
     Ok(troop.find_human_yell())
-}*/
+}
 
 #[derive(Clone)]
 pub struct MonkeyTroop {
     monkeys: BTreeMap<String,Monkey>,
     root_name: String,
+    term2lefts: BTreeMap<String,Vec<String>>,
+}
+
+fn add_left_of(on_left_of: &mut BTreeMap<String,Vec<String>>, right: &str, left: &str) {
+    match on_left_of.get_mut(left) {
+        Some(v) => {v.push(right.to_owned());}
+        None => {on_left_of.insert(left.to_owned(), vec![right.to_owned()]);}
+    }
 }
 
 impl MonkeyTroop {
     pub fn from_file(filename: &str) -> anyhow::Result<Self> {
         let mut monkeys = BTreeMap::new();
+        let mut term2lefts = BTreeMap::new();
         for line in all_lines(filename)? {
             let mut parts = line.split_whitespace();
             let name = parts.next().unwrap();
@@ -36,42 +46,95 @@ impl MonkeyTroop {
             let formula: Vec<String> = parts.map(|p| p.to_string()).collect();
             let monkey = match formula.len() {
                 1 => Monkey::Value(formula[0].parse::<i64>()?),
-                3 => Monkey::Oper(formula[0].clone(), formula[1].parse::<Sym>()?, formula[2].clone()),
+                3 => {
+                    add_left_of(&mut term2lefts, name, formula[0].as_str());
+                    add_left_of(&mut term2lefts, name, formula[2].as_str());
+                    Monkey::Oper(formula[0].clone(), formula[1].parse::<Sym>()?, formula[2].clone())
+                }
                 err => panic!("formula has {err} terms; not allowed")
             };
             monkeys.insert(name.to_owned(), monkey);
         }
-        Ok(Self {root_name: "root".to_owned(), monkeys})
+        Ok(Self {root_name: "root".to_owned(), monkeys, term2lefts})
     }
 
     fn root_monkey(&self) -> Monkey {
         self.monkeys.get(self.root_name.as_str()).unwrap().clone()
     }
 
-/* 
     pub fn find_human_yell(&self) -> i64 {
         let (t1, t2) = self.split_troop();
         let (human_troop, other_troop) = if t1.has_human() {(&t1, &t2)} else {(&t2, &t1)};
         let goal = other_troop.evaluate_root();
-        human_troop.solve_human()
+        human_troop.solve_human(goal)
     }
-    
+
+    fn reachable_names(&self) -> BTreeSet<String> {
+        let mut result = BTreeSet::new();
+        self.reachable_help(self.root_name.as_str(), &mut result);
+        result
+    }
+
+    fn reachable_help(&self, name: &str, reachable_names: &mut BTreeSet<String>) {
+        reachable_names.insert(name.to_owned());
+        match self.monkeys.get(name).unwrap() {
+            Monkey::Value(_) => {}
+            Monkey::Oper(m1, _, m2) => {
+                self.reachable_help(m1.as_str(), reachable_names);
+                self.reachable_help(m2.as_str(), reachable_names);
+            }
+        }
+    }
+
     pub fn has_human(&self) -> bool {
-        self.name2index.contains_key("humn")
+        self.reachable_names().contains("humn")
     }
 
-    fn solve_human(&self) -> i64 {
+    fn solve_human(&self, goal: i64) -> i64 {
+        let mut unknown = self.reachable_names();
+        let mut known = BTreeMap::new();
+        known.insert(self.root_name.clone(), goal);
+        for name in unknown.iter() {
+            if let Monkey::Value(v) = self.monkeys.get(name).unwrap() {
+                if name != "humn" {
+                    known.insert(name.clone(), *v);
+                }
+            }
+        }
 
+        for name in known.keys() {
+            unknown.remove(name);
+        }
+
+        let mut unknown: VecDeque<String> = unknown.iter().cloned().collect();
+        while let Some(candidate) = unknown.pop_front() {
+            let lhs = self.term2lefts.get(candidate.as_str()).unwrap();
+            assert_eq!(lhs.len(), 1);
+            let left_monkey = self.monkeys.get(lhs[0].as_str()).unwrap();
+            let (op_left, op_right) = left_monkey.names().unwrap();
+            assert!(op_left == candidate || op_right == candidate);
+            if op_left == candidate && known.contains_key(op_right.as_str()) && known.contains_key(lhs[0].as_str()) {
+                let value = left_monkey.sym().unwrap().solve_left(*known.get(lhs[0].as_str()).unwrap(), *known.get(op_right.as_str()).unwrap());
+                known.insert(candidate, value);
+            } else if op_right == candidate && known.contains_key(op_left.as_str()) && known.contains_key(lhs[0].as_str()) {
+                let value = left_monkey.sym().unwrap().solve_right(*known.get(lhs[0].as_str()).unwrap(), *known.get(op_left.as_str()).unwrap());
+                known.insert(candidate, value);
+            } else {
+                unknown.push_back(candidate);
+            }
+        }
+        *known.get("humn").unwrap()
     }
 
     pub fn split_troop(&self) -> (Self, Self) {
         let mut left = self.clone();
         let mut right = self.clone();
         let root_monkey = self.root_monkey();
-        left.root_name = 
+        let (left_name, right_name) = root_monkey.names().unwrap();
+        left.root_name = left_name;
+        right.root_name = right_name;
         (left, right)
     }
-    */
 
     pub fn evaluate_root(&self) -> i64 {
         self.root_monkey().eval(self)
@@ -102,6 +165,13 @@ impl Monkey {
             Self::Oper(m1, _, m2) => Some((m1.clone(), m2.clone()))
         }
     }
+
+    pub fn sym(&self) -> Option<Sym> {
+        match self {
+            Self::Value(_) => None,
+            Self::Oper(_, s, _) => Some(*s)
+        }
+    }
 }
 
 #[derive(Copy, Clone, PartialEq, Eq, Debug)]
@@ -116,6 +186,24 @@ impl Sym {
             Sym::Minus => a - b,
             Sym::Times => a * b,
             Sym::Divide => a / b,
+        }
+    }
+
+    pub fn solve_left(&self, result: i64, right: i64) -> i64 {
+        match self {
+            Sym::Plus => result - right,
+            Sym::Minus => result + right,
+            Sym::Times => result / right,
+            Sym::Divide => result * right,
+        }
+    }
+
+    pub fn solve_right(&self, result: i64, left: i64) -> i64 {
+        match self {
+            Sym::Plus => result - left,
+            Sym::Minus => left - result,
+            Sym::Times => result / left,
+            Sym::Divide => left / result,
         }
     }
 }
@@ -133,3 +221,29 @@ impl FromStr for Sym {
         })
     }
 }
+
+/* 
+Part 2 notes 
+
+Relevant lines:
+
+ptdq: humn - dvpt
+dvpt: 3
+lgvd: ljgn * ptdq
+cczh: sllz + lgvd
+pppw: cczh / lfqf
+lfqf: 4
+ljgn: 2
+
+pppw is at root
+
+humn = ptdq + dvpt (3)
+ptdq = lgvd / ljgn (2)
+lgvd = cczh - sllz (4)
+cczh = pppw (150) * lfqf (4)
+
+150 * 4 = 600
+600 - 4 = 596
+596 / 2 = 298
+298 + 3 = 301
+*/
