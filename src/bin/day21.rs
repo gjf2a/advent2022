1cea46b82ef2ad3b1af6bdc09070a951527c5832
gjf2a <ferrer@hendrix.edu>
2022-12-21 08:13:07 -0600

    Simplified MonkeyTroop to help with Part 2.

--- src/bin/day21.rs
+++ src/bin/day21.rs
@@ -15,55 +15,91 @@ pub fn part1(filename: &str) -> anyhow::Result<i64> {
     Ok(troop.evaluate_root())
 }
 
+/*pub fn part2(filename: &str) -> anyhow::Result<i64> {
+    let troop = MonkeyTroop::from_file(filename)?;
+    Ok(troop.find_human_yell())
+}*/
+
+#[derive(Clone)]
 pub struct MonkeyTroop {
-    name2index: BTreeMap<String,usize>,
-    monkeys: Vec<Monkey>,
+    monkeys: BTreeMap<String,Monkey>,
+    root_name: String,
 }
 
 impl MonkeyTroop {
     pub fn from_file(filename: &str) -> anyhow::Result<Self> {
-        let mut start_map = BTreeMap::new();
+        let mut monkeys = BTreeMap::new();
         for line in all_lines(filename)? {
             let mut parts = line.split_whitespace();
             let name = parts.next().unwrap();
             let name = &name[..4];
-            start_map.insert(name.to_string(), parts.map(|p| p.to_string()).collect::<Vec<String>>());
-        }
-        let names_only: Vec<String> = start_map.keys().cloned().collect();
-        let name2index: BTreeMap<String, usize> = names_only.iter().enumerate().map(|(i, s)| (s.clone(), i)).collect();
-
-        let mut monkeys = vec![];
-        for formula in start_map.values() {
+            let formula: Vec<String> = parts.map(|p| p.to_string()).collect();
             let monkey = match formula.len() {
                 1 => Monkey::Value(formula[0].parse::<i64>()?),
-                3 => Monkey::Oper(*name2index.get(&formula[0]).unwrap(), formula[1].parse::<Sym>()?, *name2index.get(&formula[2]).unwrap()),
+                3 => Monkey::Oper(formula[0].clone(), formula[1].parse::<Sym>()?, formula[2].clone()),
                 err => panic!("formula has {err} terms; not allowed")
             };
-            monkeys.push(monkey);
+            monkeys.insert(name.to_owned(), monkey);
         }
-        Ok(Self {name2index, monkeys})
+        Ok(Self {root_name: "root".to_owned(), monkeys})
     }
+
+    fn root_monkey(&self) -> Monkey {
+        self.monkeys.get(self.root_name.as_str()).unwrap().clone()
+    }
+
+/* 
+    pub fn find_human_yell(&self) -> i64 {
+        let (t1, t2) = self.split_troop();
+        let (human_troop, other_troop) = if t1.has_human() {(&t1, &t2)} else {(&t2, &t1)};
+        let goal = other_troop.evaluate_root();
+        human_troop.solve_human()
+    }
+    
+    pub fn has_human(&self) -> bool {
+        self.name2index.contains_key("humn")
+    }
+
+    fn solve_human(&self) -> i64 {
+
+    }
+
+    pub fn split_troop(&self) -> (Self, Self) {
+        let mut left = self.clone();
+        let mut right = self.clone();
+        let root_monkey = self.root_monkey();
+        left.root_name = 
+        (left, right)
+    }
+    */
 
     pub fn evaluate_root(&self) -> i64 {
-        self.eval_monkey_at(*self.name2index.get("root").unwrap())
+        self.root_monkey().eval(self)
     }
 
-    pub fn eval_monkey_at(&self, monkey: usize) -> i64 {
-        self.monkeys[monkey].eval(self)
+    pub fn eval_monkey_at(&self, monkey_name: &str) -> i64 {
+        self.monkeys.get(monkey_name).unwrap().eval(self)
     }
 }
 
-#[derive(Copy, Clone, PartialEq, Eq, Debug)]
+#[derive(Clone, PartialEq, Eq, Debug)]
 pub enum Monkey {
     Value(i64),
-    Oper(usize, Sym, usize)
+    Oper(String, Sym, String)
 }
 
 impl Monkey {
     pub fn eval(&self, troop: &MonkeyTroop) -> i64 {
         match self {
             Self::Value(v) => *v,
-            Self::Oper(m1, s, m2) => s.eval(troop.eval_monkey_at(*m1), troop.eval_monkey_at(*m2)),
+            Self::Oper(m1, s, m2) => s.eval(troop.eval_monkey_at(m1.as_str()), troop.eval_monkey_at(m2.as_str())),
+        }
+    }
+
+    pub fn names(&self) -> Option<(String,String)> {
+        match self {
+            Self::Value(_) => None,
+            Self::Oper(m1, _, m2) => Some((m1.clone(), m2.clone()))
         }
     }
 }
