8bcd0bfbccf6a2dd7c682c651b5175825eeaaedc
gjf2a <ferrer@hendrix.edu>
2022-12-25 00:04:21 -0600

    Part 1 implemented. Correct on example, fails on puzzle input.

--- src/bin/day25.rs
+++ src/bin/day25.rs
@@ -1,6 +1,7 @@
 use std::{str::FromStr, fmt::Display};
 
 use advent_code_lib::{simpler_main, all_lines};
+use bare_metal_modulo::OffsetNumC;
 
 
 
@@ -17,6 +18,7 @@ fn part1(filename: &str) -> anyhow::Result<Snafu> {
     for line in all_lines(filename)? {
         result += line.parse().unwrap();
     }
+    println!("(place, digit): {:?}", ascend_place_digit(result.0));
     Ok(result)
 }
 
@@ -45,8 +47,71 @@ impl FromStr for Snafu {
     }
 }
 
-impl Display for Snafu {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", self.0)
+fn ascend_place_digit(value: isize) -> (u32,isize) {
+    let mut opt1 = 1;
+    let mut opt2 = 2;
+    let mut place = 0;
+    loop {
+        if opt1 >= value {
+            return (place, 1);
+        } else if opt2 >= value {
+            return (place, 2);
+        }
+        opt1 *= 5;
+        opt2 *= 5;
+        place += 1;
     }
 }
+
+impl Display for Snafu {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let (mut place, digit) = ascend_place_digit(self.0);
+        write!(f, "{digit}")?;
+        let mut value = digit * 5_isize.pow(place);
+        while place > 0 {
+            place -= 1;
+            let adjust = 5_isize.pow(place);
+            let mut digit = 0;
+            if value < self.0 {
+                value += adjust;
+                digit += 1;
+                if value < self.0 {
+                    value += adjust;
+                    digit += 1;
+                }
+            } else if value > self.0 {
+                value -= adjust;
+                digit -= 1;
+                if value > self.0 {
+                    value -= adjust;
+                    digit -= 1;
+                }
+            }
+            match digit {
+                -2 => write!(f, "=")?,
+                -1 => write!(f, "-")?,
+                0..=2 => write!(f, "{digit}")?,
+                err => panic!("Erroneous digit: {err}"),    
+            }
+        }
+        Ok(())
+    }
+}
+
+/*
+4890
+
+2 * 3125 = 6250
+"2"
+6250 - 625 = 5625
+5625 - 625 = 5000
+"2="
+5000 - 125 = 4875
+"2=-"
+4875 + 25 = 4900
+"2=-1"
+4900 - 5 = 4895
+4895 - 5 = 4890
+"2=-1="
+"2=-1=0"
+*/
